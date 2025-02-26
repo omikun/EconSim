44ca6c1c56a994bed41d14c1e0dd01af11f3e8e9
Si Li <omikun@gmail.com>
2025-02-26 15:21:04 -0800

    added famine/mine collapse
    doesn't work too well as prices only adjust on trade; so every round someone needs to ask 1 commodity (no mechanism to govern this)

--- Assets/Scripts/AuctionHouseExternalities.cs
+++ Assets/Scripts/AuctionHouseExternalities.cs
@@ -43,4 +43,62 @@ public partial class AuctionHouse
 
 		forestFire = false;
 	}
+	
+	bool mineCollapse = false;
+	[HideIf("mineCollapse")]
+	[Button(ButtonSizes.Large), GUIColor(0.4f, 0.8f,1)]
+	public void MineCollapse()
+	{
+		//TODO rapid decline (*.6 every round) for 2-5 rounds, then regrow at 1.1 until reaches back to 1 multiplier
+		//do this in a new class
+		var ore = district.book["Ore"];
+		var weight = ore.productionMultiplier;
+		weight = .5f;
+
+		ore.ChangeProductionMultiplier(weight);
+		ore.productionChance = 0.5f;
+
+		mineCollapse = true;
+	}
+	[ShowIf("mineCollapse")]
+	[Button(ButtonSizes.Large), GUIColor(1, 0.4f, 0.4f)]
+	public void StopMineCollapse()
+	{
+		var ore = district.book["Ore"];
+		var weight = ore.productionMultiplier;
+		weight = 1f;
+		ore.ChangeProductionMultiplier(weight);
+		ore.productionChance = 1f;
+
+		mineCollapse = false;
+	}
+	
+	bool famine = false;
+	[HideIf("famine")]
+	[Button(ButtonSizes.Large), GUIColor(0.4f, 0.8f,1)]
+	public void Famine()
+	{
+		//TODO rapid decline (*.6 every round) for 2-5 rounds, then regrow at 1.1 until reaches back to 1 multiplier
+		//do this in a new class
+		var food = district.book["Food"];
+		var weight = food.productionMultiplier;
+		weight = .5f;
+
+		food.ChangeProductionMultiplier(weight);
+		food.productionChance = 0.5f;
+
+		famine = true;
+	}
+	[ShowIf("famine")]
+	[Button(ButtonSizes.Large), GUIColor(1, 0.4f, 0.4f)]
+	public void StopFamine()
+	{
+		var food = district.book["Food"];
+		var weight = food.productionMultiplier;
+		weight = 1f;
+		food.ChangeProductionMultiplier(weight);
+		food.productionChance = 1f;
+
+		famine = false;
+	}
 }
